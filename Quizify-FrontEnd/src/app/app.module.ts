881ea58f09cbcdeac1a26da7e3fe033a1ae3dba6
pricella10 <chintusonu55@gmail.com>
2019-02-11 18:02:34 +0530

    partial code of frontend added

--- Quizify-FrontEnd/src/app/app.module.ts
+++ Quizify-FrontEnd/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
+import { MatCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
