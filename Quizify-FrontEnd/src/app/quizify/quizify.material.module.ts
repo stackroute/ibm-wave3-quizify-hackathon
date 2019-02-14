c6be27c9fc63c0175bdce72cc80160e836619837
pricella10 <chintusonu55@gmail.com>
2019-02-14 18:07:58 +0530

    search component added

--- Quizify-FrontEnd/src/app/quizify/quizify.material.module.ts
+++ Quizify-FrontEnd/src/app/quizify/quizify.material.module.ts
@@ -9,13 +9,8 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material';
 import {MatDialogModule} from '@angular/material/dialog';
-// import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
-
-
-// import { DailougeComponent } from '../components/dailouge/dailouge.component';
-
 
 
 @NgModule({
--- Quizify-FrontEnd/src/app/quizify/quizify.module.ts
+++ Quizify-FrontEnd/src/app/quizify/quizify.module.ts
@@ -6,10 +6,12 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { CardsComponent } from './components/cards/cards.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material';
+import { SearchComponent } from './components/search/search.component';
 @NgModule({
   declarations: [ CardsComponent,
     ProfileComponent,
-    RegisterComponent],
+    RegisterComponent,
+    SearchComponent],
   imports: [
     CommonModule,
     BrowserAnimationsModule,
@@ -19,7 +21,8 @@ import { MatCardModule } from '@angular/material';
   ],
   exports: [ CardsComponent,
     ProfileComponent,
-    RegisterComponent ]
+    RegisterComponent,
+    SearchComponent ]
 
 })
 export class QuizifyModule { }
