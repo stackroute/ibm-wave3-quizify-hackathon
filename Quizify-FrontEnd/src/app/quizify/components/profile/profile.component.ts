00f0837c0a6c2420c3f2ff87870425e99a7edf52
Anjali <anjsin21@in.ibm.com>
2019-02-15 12:25:27 +0530

    changes in profile component

--- Quizify-FrontEnd/src/app/quizify/components/profile/profile.component.ts
+++ Quizify-FrontEnd/src/app/quizify/components/profile/profile.component.ts
@@ -1,10 +1,7 @@
-// import { ProfileuserComponent } from './../profileuser/profileuser.component';
 import { Component, OnInit, Inject, Input, Output, EventEmitter } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
 import { Route } from '@angular/compiler/src/core';
 import { RouterLink, ActivatedRoute, Router } from '@angular/router';
-import { ProfileuserComponent } from '../profileuser/profileuser.component';
-
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
