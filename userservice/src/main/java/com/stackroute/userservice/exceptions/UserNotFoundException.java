a4bbf7ed9fce294f4f1f13a9da6abc8e1bfd33b2
yeakhil1 <yeakhil1@in.ibm.com>
2019-02-16 17:24:07 +0530

    register component

--- userservice/src/main/java/com/stackroute/userservice/exceptions/UserNotFoundException.java
+++ userservice/src/main/java/com/stackroute/userservice/exceptions/UserNotFoundException.java
@@ -1,14 +1,5 @@
 package com.stackroute.userservice.exceptions;
 
-//public class UpdateException extends Exception {
-//    private String message;
-//    public UpdateException(){}
-//
-//    public UpdateException(String message){
-//        super(message);
-//        this.message=message;
-//    }
-//}
 
 public class UserNotFoundException extends Exception
 {
