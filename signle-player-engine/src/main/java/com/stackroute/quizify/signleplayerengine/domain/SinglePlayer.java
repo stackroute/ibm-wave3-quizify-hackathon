b0e069a6204d0d1e436f19816644bfd0796b7af1
kaustavpal <kaustavlogan@gmail.com>
2019-02-22 15:21:42 +0530

    Edited Single Player Engine

--- signle-player-engine/src/main/java/com/stackroute/quizify/signleplayerengine/domain/SinglePlayer.java
+++ signle-player-engine/src/main/java/com/stackroute/quizify/signleplayerengine/domain/SinglePlayer.java
@@ -2,12 +2,15 @@ package com.stackroute.quizify.signleplayerengine.domain;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@Document(collection = "players")
 @Data
 public class SinglePlayer {
     @Id
+    private long id;
     private Game game;
     private List<User> users;
 }
--- signle-player-engine/src/main/java/com/stackroute/quizify/signleplayerengine/repository/PlayerRepository.java
+++ signle-player-engine/src/main/java/com/stackroute/quizify/signleplayerengine/repository/PlayerRepository.java
@@ -1,10 +1,11 @@
 package com.stackroute.quizify.signleplayerengine.repository;
 
+import com.stackroute.quizify.signleplayerengine.domain.SinglePlayer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlayerRepository extends MongoRepository {
+public interface PlayerRepository extends MongoRepository<SinglePlayer, Long> {
 
 
 
